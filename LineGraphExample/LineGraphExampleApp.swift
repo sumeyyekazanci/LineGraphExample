3ed111a71f21f64e6d19bd2c039bb249b5c22301
Smyy <sumeyyekazanci13@gmail.com>
2022-07-12 21:05:38 +0300

    Initial Commit

---
+++ LineGraphExample/LineGraphExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineGraphExampleApp.swift
+//  LineGraphExample
+//
+//  Created by Sümeyye Kazancı on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LineGraphExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
